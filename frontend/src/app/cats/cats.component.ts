0f4915c40223dc72b60995ee459dc43d7db88e76
ThiagoMartinsSaraiva <thiagomartinssaraiva@gmail.com>
2020-02-03 23:24:21 -0300

    adding loading

--- frontend/src/app/cats/cats.component.ts
+++ frontend/src/app/cats/cats.component.ts
@@ -14,19 +14,24 @@ export class CatsComponent implements OnInit {
   ) { }
 
   cats: Cat[] = []
+  isLoading = false
 
   ngOnInit() {
+    this.isLoading = true
     this.service.getAll().subscribe(cats => {
       cats.forEach(cat => {
         this.cats.push(cat)
       })
+      this.isLoading = false
     })
   }
 
   removeCat(_id: string): void {
+    this.isLoading = true
     this.service.remove(_id).subscribe(res => {
       let catIndex = this.cats.findIndex(cat => cat._id == _id)
       this.cats.splice(catIndex, 1)
+      this.isLoading = false
     })
   }
 
